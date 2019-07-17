948d0c1b594678bd072aa0f24c77b900b61245e1
Samuel Rowe <samuelrowe1999@gmail.com>
2019-07-17 16:25:24 +0530

    Added an interface to the compiler.

---
+++ include/com/onecube/zen/compiler/Compiler.h
@@ -0,0 +1,24 @@
+// Wednesday, July 17, 2019
+
+#ifndef COM_ONECUBE_ZEN_COMPILER_COMPILER_H
+#define COM_ONECUBE_ZEN_COMPILER_COMPILER_H
+
+/*******************************************************************************
+ * Compiler                                                                    *
+ *******************************************************************************/
+
+/**
+ * @class Compiler
+ * @ingroup zen_compiler
+ * @author Samuel Rowe
+ * @since zen 1.0
+ */
+struct zen_Compiler_t {
+};
+
+/**
+ * @memberof Compiler
+ */
+typedef zen_Compiler_t zen_Compiler_t;
+
+#endif /* COM_ONECUBE_ZEN_COMPILER_COMPILER_H */
