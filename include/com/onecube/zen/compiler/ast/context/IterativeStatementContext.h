8db011cc101076e5a67a3c7bd21f7a2aafc53c64
Samuel Rowe <samuelrowe1999@gmail.com>
2019-08-21 20:05:18 +0530

    Added license comments, the license file, and a shell script for inserting license comments.

--- include/com/onecube/zen/compiler/ast/context/IterativeStatementContext.h
+++ include/com/onecube/zen/compiler/ast/context/IterativeStatementContext.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018-2019 OneCube
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Wednesday, July 17, 2019
 
 #ifndef COM_ONECUBE_ZEN_COMPILER_AST_CONTEXT_ITERATIVE_STATEMENT_CONTEXT_H
