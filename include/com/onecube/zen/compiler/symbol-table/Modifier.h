38878914d4c2332d87eb4cba0a3a5aa6df67dab5
Samuel Rowe <samuelrowe1999@gmail.com>
2019-11-23 22:48:45 +0530

    Added public, private, protected, synthetic, abstract, final, static, native, annotation, enumeration, and variable parameter modifiers to the zen_Modifier_t enumeration.

--- include/com/onecube/zen/compiler/symbol-table/Modifier.h
+++ include/com/onecube/zen/compiler/symbol-table/Modifier.h
@@ -1,12 +1,12 @@
 /*
  * Copyright 2018-2019 OneCube
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,7 +30,28 @@
  * @since zen 1.0
  */
 enum zen_Modifier_t {
-    ZEN_MODIFIER_VARIABLE_PARAMETER = 1 << 0
+
+    ZEN_MODIFIER_PUBLIC = 1 << 0,
+
+    ZEN_MODIFIER_PRIVATE = 1 << 1,
+
+    ZEN_MODIFIER_PROTECTED = 1 << 2,
+
+    ZEN_MODIFIER_SYNTHETIC = 1 << 3,
+
+    ZEN_MODIFIER_ABSTRACT = 1 << 4,
+
+    ZEN_MODIFIER_FINAL = 1 << 5,
+
+    ZEN_MODIFIER_STATIC = 1 << 6,
+
+    ZEN_MODIFIER_NATIVE = 1 << 7,
+
+    ZEN_MODIFIER_ANNOTATION = 1 << 8,
+
+    ZEN_MODIFIER_ENUMERATION = 1 << 9,
+
+    ZEN_MODIFIER_VARIABLE_PARAMETER = 1 << 10
 };
 
 /**
